69f1b7522555d30c8cfe7f0b96d3212e09167953
Valentin <vflorea97@yahoo.com>
2023-01-28 15:47:17 +0200

    upload project

---
+++ src/main/java/ro/mycode/magazinmanagement/MagazinManagementApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.magazinmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazinManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazinManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/magazinmanagement/MagazinManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.magazinmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazinManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

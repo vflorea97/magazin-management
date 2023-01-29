83b5f02ce7e1a536213cb42febf45b8ff4f802f0
Valentin <vflorea97@yahoo.com>
2023-01-29 15:23:31 +0200

    create repository and repositoryTest

---
+++ src/main/java/ro/mycode/magazinmanagement/Repository/MagazinRepository.java
@@ -0,0 +1,37 @@
+package ro.mycode.magazinmanagement.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ro.mycode.magazinmanagement.Model.Magazin;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MagazinRepository extends JpaRepository<Magazin, Long> {
+
+    @Query("select m from Magazin m where m.anInfiintare > ?1")
+    Optional<List<Magazin>> getMagazinByAnInfiintare(int an);
+
+    @Query("select m from Magazin m where m.descriere like %?1")
+    Optional<List<Magazin>> getMagazinByDescriere(String terminatie);
+
+    @Query("select count(m.nume) from Magazin m where substr(m.nume,1,1) in (?1)")
+    int getTotalMagazineCareIncepCu(char litera);
+
+    @Query("select count(m.nume),m.culoareLogo from Magazin m group by m.culoareLogo")
+    String [] getMagazinByCuloareLogo();
+
+    Optional<Magazin> findByNumarFiscal(int numarFiscal);
+
+    @Transactional
+    Optional<Magazin> removeMagazinByNumarFiscal(int numarFiscal);
+
+    @Transactional
+    @Query("update Magazin m set m.numarAngajati = ?1 where m.numarFiscal = ?2")
+    void updateNumarAngajati(int numarAngajati, int numarFiscal);
+
+
+}
